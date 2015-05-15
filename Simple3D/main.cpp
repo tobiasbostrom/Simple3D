95cebba143df23227d1d879aaf527096b64f8012
Tobias Boström <bostrom.tobias@gmail.com>
2015-05-15 22:59:02 +0200

    Initial commit from local git

---
+++ Simple3D/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Simple3D
+//
+//  Created by Tobias Boström on 15/05/15.
+//  Copyright (c) 2015 Tobias Boström. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
